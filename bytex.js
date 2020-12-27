d6299804c32f6e6642562f8fd2fad03408c9ed5a
BrainyXS <brainyxs.gmail.com>
2020-12-27 21:57:52 +0100

    Working with Bugs

--- bytex.js
+++ bytex.js
@@ -12,15 +12,7 @@ function loaded(youtubelive) {
             text = text.replace("TestEmote", "<img src=https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/1x >")
             document.getElementById(id).getElementsByTagName("div")[0].getElementsByClassName("yt-live-chat-text-message-renderer")[2].innerHTML = text;
             window.setTimeout(function () {
-                let id = message.id.toString();
-                let element = document.getElementById(id);
-                let innerElement = element.getElementsByTagName("div")[0].getElementsByClassName("yt-live-chat-text-message-renderer")[2];
-                if (innerElement === undefined) {
-                    return;
-                }
-                let text = innerElement.innerHTML;
 
-                text = text.replace("TestEmote", "<img src=https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/1x >")
                 document.getElementById(id).getElementsByTagName("div")[0].getElementsByClassName("yt-live-chat-text-message-renderer")[2].innerHTML = text;
 
             }, 1000);
